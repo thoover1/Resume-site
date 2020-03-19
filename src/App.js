f57349b9a950be6a3504f211bef091512941c579
thoover1 <thoover1@uab.edu>
2020-03-19 12:46:21 -0500

    deleted background of resume and added link to portfolio

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from "react";
 import Header from "./Header/Header";
-import background1 from "./background/Resume1.png";
-import background2 from "./background/Resume2.png";
-import background3 from "./background/Resume3.png";
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div>
-        <img src={background1} alt="" />
-        <img src={background2} alt="" />
-        <img src={background3} alt="" />
-      </div>
+      <a className="link" href="https://thomashooverdev.netlify.com/">
+        Back to Portfolio
+      </a>
     </div>
   );
 }
